2ef32247fb68a0376550748467ee65c5b324ea99
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-07-22 18:06:14 -0700

    [FEATURE] Import bitcoinjs

---
+++ README.md
@@ -0,0 +1,17 @@
+# Example contract that uses bitcoinjs
+
+This repository contains an example contract showing how to use `bitcoinjs` in Codius.
+
+To install the Codius command-line interface (CLI), run:
+
+``` sh
+sudo npm install -g codius
+```
+
+To try this example contract, run:
+
+``` sh
+git clone https://github.com/codius/example-bitcoin.git codius-example-bitcoin
+cd codius-example-bitcoin
+codius run
+```
---
+++ contract.js
@@ -0,0 +1,19 @@
+console.log('example-bitcoin');
+
+var Bitcoin = require('bitcoinjs');
+
+// var key = Bitcoin.ECKey.makeRandom();
+//
+// console.log('WIF', key.toWIF());
+//
+// console.log(key.pub.getAddress().toString());
+//
+// var tx = new Bitcoin.Transaction();
+//
+// tx.addInput("aa94ab02c182214f090e99a0d57021caffd0f195a81c24602b1028b130b63e31", 0);
+//
+// tx.addOutput("1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK", 15000);
+//
+// tx.sign(0, key);
+//
+// console.log(tx.serializeHex());
