052fa0924bfae975719db96bfbb536512da6a64f
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-07-25 15:59:16 -0700

    [FEATURE] Create and sign bitcoin transaction
    Had to modify lines 2125 and 2543 of bitcoinjs.js to avoid errors window.crypto undefined and DataView undefined.
    This is hacky and should be fixed.

--- contract.js
+++ contract.js
@@ -2,18 +2,14 @@ console.log('example-bitcoin');
 
 var Bitcoin = require('bitcoinjs');
 
-// var key = Bitcoin.ECKey.makeRandom();
-//
-// console.log('WIF', key.toWIF());
-//
-// console.log(key.pub.getAddress().toString());
-//
-// var tx = new Bitcoin.Transaction();
-//
-// tx.addInput("aa94ab02c182214f090e99a0d57021caffd0f195a81c24602b1028b130b63e31", 0);
-//
-// tx.addOutput("1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK", 15000);
-//
-// tx.sign(0, key);
-//
-// console.log(tx.serializeHex());
+var key = Bitcoin.ECKey.fromWIF('5J5CvkZJiw5H1WdRx52ZtysSmaRrKmUN4NXKWkXwx8zaAvpgMEB');
+
+var tx = new Bitcoin.Transaction();
+
+tx.addInput("aa94ab02c182214f090e99a0d57021caffd0f195a81c24602b1028b130b63e31", 0);
+
+tx.addOutput("1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK", 15000);
+
+tx.sign(0, key);
+
+console.log(tx.toHex());
